a8e32f819d2dbebdd7adb663643daffe2b646c8e
mancunian1792 <linkinnation1792@gmail.com>
2019-04-14 01:31:35 -0400

    Plot the trajectory and visualize it .

--- initialize.m
+++ initialize.m
@@ -24,6 +24,11 @@ qMilestones = rrt(rob,qStart,qGoal,qMin,qMax,sphere1Center,sphereRadius, sphere2
 % Plot robot following path
 qTraj = interpMilestones(qMilestones);
 rob.plot(qTraj);
+positions = rob.fkine(qTraj);
+for i=1:length(qTraj)
+    pos = positions(1:3,4,i);
+    plot3(pos(1), pos(2), pos(3), '*r')
+end
  
 function drawSphere(position,diameter)
 
@@ -38,6 +43,100 @@ function drawSphere(position,diameter)
     surf(X,Y,Z);
     %~ shading flat
 
+end
+
+% 
+% Evaluate whether the configuration <q> is in collision with a spherical
+% obstacle centered at <sphereCenter> with radius <r>.
+% 
+% input: q -> 1x4 vector of joint angles
+%        sphereCenter -> 3x1 vector that denotes sphere center
+%        r -> radius of sphere 
+% output: collision -> binary number that denotes whether this
+%                      configuration is in collision or not.
+function collision = robotCollision(rob,q,sphereCenter,r)
+
+    % get points on elbow and at EE
+    x1 = [0;0;0];
+    T2 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q);
+    x2 = T2(1:3,4);
+    T3 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q) * rob.A(4,q);
+    x3 = T3(1:3,4);
+    
+    vec = 0:0.1:1;
+    m = size(vec,2);
+    
+    x12 = repmat(x2-x1,1,m) .* repmat(vec,3,1) + repmat(x1,1,m);
+    x23 = repmat(x3-x2,1,m) .* repmat(vec,3,1) + repmat(x2,1,m);
+    x = [x12 x23];
+    
+    if sum(sum((x - repmat(sphereCenter,1,size(x,2))).^2,1) < r^2) > 0
+        collision = 1;
+    else
+        collision = 0;
+    end
+
+end
+
+
+
+function traj = interpMilestones(qMilestones)
+
+    d = 0.05;
+%     traj = qMilestones(1,:);
+    traj = [];
+    for i=2:size(qMilestones,1)
+        
+        delta = qMilestones(i,:) - qMilestones(i-1,:);
+        m = max(floor(norm(delta) / d),1);
+        vec = linspace(0,1,m);
+        leg = repmat(delta',1,m) .* repmat(vec,size(delta,2),1) + repmat(qMilestones(i-1,:)',1,m);
+        traj = [traj;leg'];
+        
+    end
+end
+
+function collision = checkCollision(rob,q,sphereCenter,r)
+
+    % get points on elbow and at EE
+    x1 = [0;0;0];
+    T2 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q);
+    x2 = T2(1:3,4);
+    T3 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q) * rob.A(4,q);
+    x3 = T3(1:3,4);
+    
+    vec = 0:0.1:1;
+    m = size(vec,2);
+    
+    x12 = repmat(x2-x1,1,m) .* repmat(vec,3,1) + repmat(x1,1,m);
+    x23 = repmat(x3-x2,1,m) .* repmat(vec,3,1) + repmat(x2,1,m);
+    x = [x12 x23];
+    
+    if sum(sum((x - repmat(sphereCenter,1,size(x,2))).^2,1) < r^2) > 0
+        collision = 1;
+    else
+        collision = 0;
+    end
+
+end
+
+function collision = checkEdge(rob,q1,q2,sphereCenter,r)
+
+    n = 10; % Num pts along vector to check
+    m = size(q1,2); % Dimension of c-space
+    
+    % Generate n pts between q1 and q2
+    viaPts = repmat(q2-q1,[n,1]) .* repmat(linspace(0,1,n)', [1 m]) + repmat(q1,[n,1]);
+    
+    % Check those pts for collision
+    collision = 0;
+    for i = 1:n
+        collision = robotCollision(rob,viaPts(i,:),sphereCenter,r);
+        if collision
+            break
+        end
+    end
+    
 end
 
 % Find a path from qStart to qGoal using RRT.
@@ -101,64 +200,7 @@ function qMilestones = rrt(rob,qStart,qGoal,qMin,qMax,sphere1Center,sphereRadius
     end 
 end
 
-function traj = interpMilestones(qMilestones)
 
-    d = 0.05;
-%     traj = qMilestones(1,:);
-    traj = [];
-    for i=2:size(qMilestones,1)
-        
-        delta = qMilestones(i,:) - qMilestones(i-1,:);
-        m = max(floor(norm(delta) / d),1);
-        vec = linspace(0,1,m);
-        leg = repmat(delta',1,m) .* repmat(vec,size(delta,2),1) + repmat(qMilestones(i-1,:)',1,m);
-        traj = [traj;leg'];
-        
-    end
-end
-
-function collision = checkCollision(rob,q,sphereCenter,r)
-
-    % get points on elbow and at EE
-    x1 = [0;0;0];
-    T2 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q);
-    x2 = T2(1:3,4);
-    T3 = rob.A(1,q) * rob.A(2,q) * rob.A(3,q) * rob.A(4,q);
-    x3 = T3(1:3,4);
-    
-    vec = 0:0.1:1;
-    m = size(vec,2);
-    
-    x12 = repmat(x2-x1,1,m) .* repmat(vec,3,1) + repmat(x1,1,m);
-    x23 = repmat(x3-x2,1,m) .* repmat(vec,3,1) + repmat(x2,1,m);
-    x = [x12 x23];
-    
-    if sum(sum((x - repmat(sphereCenter,1,size(x,2))).^2,1) < r^2) > 0
-        collision = 1;
-    else
-        collision = 0;
-    end
-
-end
-
-function collision = checkEdge(rob,q1,q2,sphereCenter,r)
-
-    n = 10; % Num pts along vector to check
-    m = size(q1,2); % Dimension of c-space
-    
-    % Generate n pts between q1 and q2
-    viaPts = repmat(q2-q1,[n,1]) .* repmat(linspace(0,1,n)', [1 m]) + repmat(q1,[n,1]);
-    
-    % Check those pts for collision
-    collision = 0;
-    for i = 1:n
-        collision = robotCollision(rob,viaPts(i,:),sphereCenter,r);
-        if collision
-            break
-        end
-    end
-    
-end
 
 
 
